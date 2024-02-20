98458490421f9d90ee9143264ed2c51dc7a0997b
MatteoIng <matteofoschi10@gmail.com>
2024-02-20 10:58:46 +0100

    Update visualizzazione.py

--- visualizzazione.py
+++ visualizzazione.py
@@ -29,11 +29,12 @@ def visualizza_reward_mosse():
     yA = []
     yB = []
 
-    for i in range(len(app)) :
+    # reward per ogni partita
+    """ for i in range(len(app)) :
         yA.append(app[i][1])
         yB.append(bpp[i][1])
-    """ print('YA:',yA)
-    print('YB:',yB) """
+    #print('YA:',yA)
+    #print('YB:',yB)
     plt.figure()
     plt.title('ADDESTRAMENTO/EVALUATE')
     plt.ylabel('reward')
@@ -42,7 +43,7 @@ def visualizza_reward_mosse():
     plt.plot(np.arange(len(yA)),yA)
     plt.plot(np.arange(len(yB)),yB)
     
-    plt.legend(['attaccante','difensore'])
+    plt.legend(['attaccante','difensore']) """
 
 
     # reward per epoca
@@ -76,59 +77,59 @@ def visualizza_reward_mosse():
     plt.ylabel('reward')
     plt.xlabel('epoche')
     #plt.xlabel('numero mosse per partita')
-    plt.plot(np.arange(len(yA)),yA)
+    #plt.plot(np.arange(len(yA)),yA)
     plt.plot(np.arange(len(yB)),yB)
     rb = [-1.4 for i in range(len(yA))]
     plt.plot(np.arange(len(yA)),rb)
-    plt.legend(['attaccante','difensore'])
+    plt.legend(['difensore','reward ottimo difensore'])
 
 
 
     # il numero di mosse fatte nel tempo, per partita
-    y = []
+    """ y = []
     for i in a :
         y.append(i[0])
     plt.figure()
     plt.title('MOSSE ATT+DIF fatte per ogni partita')
     plt.ylabel('n mosse')
     plt.xlabel('partite')
-    plt.plot(np.arange(len(y)),y)
+    plt.plot(np.arange(len(y)),y) """
 
 
 
 
     # reward rispetto al numero di mosse fatte dall'attaccante
-    x = []
+    """ x = []
     y = []
     a.sort()
     for i in a :
         x.append(i[0])
         y.append(i[1])
-    """ print(len(x))
-    print(len(y)) """
+    #print(len(x))
+    #print(len(y))
     plt.figure()
     plt.title('REWARD attaccante rispetto il N.MOSSE')
     plt.ylabel('reward attaccante')
     plt.xlabel('numero mosse per partita')
-    plt.plot(x,y)
+    plt.plot(x,y) """
 
     # Grafico 3
     # reward rispetto al numero di mosse fatte dal difensore
-    x = []
+    """ x = []
     y = []
     b.sort()
     for i in b :
         x.append(i[0])
         y.append(i[1])
-    """ print(len(x))
-    print(len(y)) """
+    #print(len(x))
+    #print(len(y))
     plt.figure()
     plt.title('')
     plt.title('REWARD difensore rispetto il N.MOSSE')
     plt.xlabel('numero mosse per partita')
-    plt.plot(x,y)
+    plt.plot(x,y) """
 
     plt.show()
 
 
-visualizza_reward_mosse()
+#visualizza_reward_mosse()
