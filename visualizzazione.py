692216bed5e833489c9f21cab6938d30d756360e
MatteoIng <matteofoschi10@gmail.com>
2024-02-28 16:12:05 +0100

    Update visualizzazione.py

--- visualizzazione.py
+++ visualizzazione.py
@@ -109,10 +109,13 @@ def visualizza_reward_mosse():
 
     # Plotta i dati
     ax.plot(tempo,yB)
+    ax.set_xlabel('secondi')
    
     ax2 = ax.twiny()
     ax2.plot(tempo,yB)
     ax2.set_xticks([0,tempo[int(len(tempo)/4)],tempo[int(len(tempo)/2)],tempo[int(3*(len(tempo))/4)],tempo[-1]],[0,int(len(yB)/4),int(len(yB)/2),int(3*(len(yB)/4)),len(yB)])
+    ax2.set_xlabel('epoche')
+
     
     plt.show()
 
